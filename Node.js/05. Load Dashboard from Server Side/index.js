1d38b8190d43b62a8abec1b94c2cc49869019891
Yuriy Kubekin <vkubekin@gmail.com>
2020-11-07 12:04:38 +0300

    Update Sample

--- Node.js/05. Load Dashboard from Server Side/index.js
+++ Node.js/05. Load Dashboard from Server Side/index.js
@@ -8,34 +8,34 @@ function accept(req, res) {
 	//Send index.html
 	if (req.url == "/") {
 		res.writeHeader(200, { "Content-Type": "text/html" });
-		res.end(fs.readFileSync("web/index.html"));
+		res.end(fs.readFileSync("index.html"));
 	}
 	//Send style.css
 	else if (req.url == "/stimulsoft.viewer.office2013.whiteblue.css") {
 		res.writeHeader(200, { "Content-Type": "text/css" });
-		res.end(fs.readFileSync("web/stimulsoft.viewer.office2013.whiteblue.css"));
+		res.end(fs.readFileSync("node_modules/stimulsoft-dashboards-js/Css/stimulsoft.viewer.office2013.whiteblue.css"));
 	}
 	//Send reports.js
 	else if (req.url == "/stimulsoft.reports.js") {
 		res.writeHeader(200, { "Content-Type": "text/javascript" });
-		res.end(fs.readFileSync("web/stimulsoft.reports.js"));
+		res.end(fs.readFileSync("node_modules/stimulsoft-dashboards-js/Scripts/stimulsoft.reports.js"));
 	}
 	//Send dashboards.js
 	else if (req.url == "/stimulsoft.dashboards.js") {
 		res.writeHeader(200, { "Content-Type": "text/javascript" });
-		res.end(fs.readFileSync("web/stimulsoft.dashboards.js"));
+		res.end(fs.readFileSync("node_modules/stimulsoft-dashboards-js/Scripts/stimulsoft.dashboards.js"));
 	}
 	//Send viewer.js
 	else if (req.url == "/stimulsoft.viewer.js") {
 		res.writeHeader(200, { "Content-Type": "text/javascript" });
-		res.end(fs.readFileSync("web/stimulsoft.viewer.js"));
+		res.end(fs.readFileSync("node_modules/stimulsoft-dashboards-js/Scripts/stimulsoft.viewer.js"));
 	}
 	//Send dashboard
 	else if (req.url == "/getDashboard") {
 		// Creating new dashboard
 		var report = Stimulsoft.Report.StiReport.createNewDashboard();
 		// Loading dashboard template
-		report.loadFile("dashboard/Dashboard.mrt");
+		report.loadFile("Dashboard.mrt");
 
 		// Saving dashboard to JSON string
 		var dashboardJson = report.saveToJsonString();
