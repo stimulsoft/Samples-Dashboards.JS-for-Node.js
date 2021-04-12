2fccfe2899c02c10c906e88cfc2466dc54834a77
Yuriy Kubekin <vkubekin@gmail.com>
2021-04-12 11:00:54 +0300

    Update Sample

--- Node.js/04. Start SQL Adapters from Http Server/index.js
+++ Node.js/04. Start SQL Adapters from Http Server/index.js
@@ -4,7 +4,6 @@ var FirebirdAdapter = require('./FirebirdAdapter');
 var MSSQLAdapter = require('./MSSQLAdapter');
 var PostgreSQLAdapter = require('./PostgreSQLAdapter');
 
-var connectionStringBuilder;
 var response;
 function accept(req, res) {
     response = res;
@@ -12,20 +11,21 @@ function accept(req, res) {
     response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
     response.setHeader("Cache-Control", "no-cache");
-    
+
     var data = "";
     req.on('data', function (buffer) {
         data += buffer;
     });
 
     req.on('end', function () {
-        if (data.indexOf("{") != 0){
-            data = Buffer.from(data.replace(/[A-Za-z]/g, function(c) {
+        if (data.indexOf("{") != 0) {
+            data = Buffer.from(data.replace(/[A-Za-z]/g, function (c) {
                 return String.fromCharCode(c.charCodeAt(0) + (c.toUpperCase() <= "M" ? 13 : -13));
             }), "base64").toString("ascii");
 
         }
         command = JSON.parse(data.toString());
+        command.queryString = applyQueryParameters(command.queryString, command.parameters, command.escapeQueryParameters);
 
         if (command.database == "MySQL") MySQLAdapter.process(command, onProcess);
         else if (command.database == "Firebird") FirebirdAdapter.process(command, onProcess);
@@ -35,7 +35,44 @@ function accept(req, res) {
     });
 }
 
-var onProcess = function (result){
+var applyQueryParameters = function (baseSqlCommand, parameters, escapeQueryParameters) {
+    if (baseSqlCommand == null || baseSqlCommand.indexOf("@") < 0) return baseSqlCommand;
+
+    var result = "";
+    while (baseSqlCommand.indexOf("@") >= 0 && parameters != null) {
+        result += baseSqlCommand.substring(0, baseSqlCommand.indexOf("@"));
+        baseSqlCommand = baseSqlCommand.substring(baseSqlCommand.indexOf("@") + 1);
+
+        var parameterName = "";
+
+        while (baseSqlCommand.length > 0) {
+            var char = baseSqlCommand.charAt(0);
+            if (char.length === 1 && char.match(/[a-zA-Z0-9_-]/i)) {
+                parameterName += char;
+                baseSqlCommand = baseSqlCommand.substring(1);
+            }
+            else break;
+        }
+
+        var parameter = parameters.find(parameter => parameter.name.toLowerCase() == parameterName.toLowerCase());
+        if (parameter) {
+            if (parameter.typeGroup != "number") {
+                if (escapeQueryParameters)
+                    result += "'" + parameter.value.toString().replace(/\\/gi, "\\\\").replace(/\'/gi, "\\\'").replace(/\"/gi, "\\\"") + "'";
+                else
+                    result += "'" + parameter.value.toString() + "'";
+            }
+            else
+                result += parameter.value.toString();
+        }
+        else
+            result += "@" + parameterName;
+    }
+
+    return result;
+}
+
+var onProcess = function (result) {
     response.end(JSON.stringify(result));
 }
 
