88d6fa9a88c8a3047c403aa8b6a4a807ee8fcd79
Tim Kubekin <tima.kubekin156@gmail.com>
2022-03-22 11:22:06 +0300

    Some fixes in samples

--- Vue.js 3/Integrating the Dashboard Designer into an Application/src/App.vue
+++ Vue.js 3/Integrating the Dashboard Designer into an Application/src/App.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { Stimulsoft } from "stimulsoft-dashboards-js/Scripts/stimulsoft.blockly.editor";
-import 'stimulsoft-dashboards-js/Css/stimulsoft.designer.office2013.whiteblue.css';
-import 'stimulsoft-dashboards-js/Css/stimulsoft.viewer.office2013.whiteblue.css';
+import "stimulsoft-dashboards-js/Css/stimulsoft.designer.office2013.whiteblue.css";
+import "stimulsoft-dashboards-js/Css/stimulsoft.viewer.office2013.whiteblue.css";
 
 onMounted(() => {
-  console.log('Loading Designer view');
+  console.log("Loading Designer view");
 
-  console.log('Set full screen mode for the designer');
-  var options = new Stimulsoft.Designer.StiDesignerOptions();
+  console.log("Set full screen mode for the designer");
+  let options = new Stimulsoft.Designer.StiDesignerOptions();
   options.appearance.fullScreenMode = false;
 
-  console.log('Create the dashboard designer with specified options');
-  var designer = new Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
+  console.log("Create the dashboard designer with specified options");
+  let designer = new Stimulsoft.Designer.StiDesigner(options, "StiDesigner", false);
 
-  console.log('Create a new dashboard instance');
-  var report = Stimulsoft.Report.StiReport.createNewDashboard();
+  console.log("Create a new dashboard instance");
+  let report = Stimulsoft.Report.StiReport.createNewDashboard();
 
-  console.log('Load dashboard from url');
-  report.loadFile('dashboard/DashboardChristmas.mrt');
+  console.log("Load dashboard from url");
+  report.loadFile("dashboard/DashboardChristmas.mrt");
 
-  console.log('Edit dashboard template in the designer');
+  console.log("Edit dashboard template in the designer");
   designer.report = report;
 
-  console.log('Rendering the designer to selected element');
-  designer.renderHtml('designer');
+  console.log("Rendering the designer to selected element");
+  designer.renderHtml("designer");
 
-  console.log('Loading completed successfully!');
+  console.log("Loading completed successfully!");
 });
 </script>
 
--- Vue.js 3/Integrating the Dashboard Viewer into an Application/src/App.vue
+++ Vue.js 3/Integrating the Dashboard Viewer into an Application/src/App.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { Stimulsoft } from "stimulsoft-dashboards-js/Scripts/stimulsoft.blockly.editor";
-import 'stimulsoft-dashboards-js/Css/stimulsoft.viewer.office2013.whiteblue.css';
+import "stimulsoft-dashboards-js/Css/stimulsoft.viewer.office2013.whiteblue.css";
 
 onMounted(() => {
-  console.log('Loading Viewer view');
+  console.log("Loading Viewer view");
     
-  console.log('Creating the dashboard viewer with default options');
-  var viewer = new Stimulsoft.Viewer.StiViewer(undefined, 'StiViewer', false);
+  console.log("Creating the dashboard viewer with default options");
+  let viewer = new Stimulsoft.Viewer.StiViewer(undefined, "StiViewer", false);
 
-  console.log('Creating a new dashboard instance');
+  console.log("Creating a new dashboard instance");
     
-  var report = Stimulsoft.Report.StiReport.createNewDashboard();
+  let report = Stimulsoft.Report.StiReport.createNewDashboard();
 
-  console.log('Load dashboard from url');
-  report.loadFile('/dashboard/DashboardChristmas.mrt');
+  console.log("Load dashboard from url");
+  report.loadFile("/dashboard/DashboardChristmas.mrt");
 
-  console.log('Assigning dashboard to the viewer, the dashboard will be built automatically after rendering the viewer');
+  console.log("Assigning dashboard to the viewer, the dashboard will be built automatically after rendering the viewer");
   viewer.report = report;
 
-  console.log('Rendering the viewer to selected element');
-  viewer.renderHtml('viewer');
+  console.log("Rendering the viewer to selected element");
+  viewer.renderHtml("viewer");
 
-  console.log('Loading completed successfully!');
+  console.log("Loading completed successfully!");
 });
 </script>
 
